a9ea136b515e27774c90e1f3621e983dd81e2902
Philipp <pd95@users.noreply.github.com>
2022-07-09 16:20:33 +0200

    Initial Commit

---
+++ AutofillPlayground/AutofillPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutofillPlaygroundApp.swift
+//  AutofillPlayground
+//
+//  Created by Philipp on 09.07.22.
+//
+
+import SwiftUI
+
+@main
+struct AutofillPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
